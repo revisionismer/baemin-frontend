8c9e9304113c1ff9dfd08768b6315c53714f52c3
revisionismer <cherry2137@naver.com>
2024-11-23 21:54:28 +0900

    1. 프로젝트 생성 및 프로젝트 구조 잡기

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../assets/css/footer.css';
+
+const Footer = () => {
+    return (
+        <div id='footer'>
+            <div className="box">
+                <div>이름</div>
+                <div>깃허브</div>
+                <div>전화번호</div>
+                <div>이메일</div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
